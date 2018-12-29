62c930777a14db5c03d677d1f31e93e693348bc9
sadanand48 <sadanand.shenoy4898@gmail.com>
2018-12-29 22:43:01 +0530

    removed

--- sll/3/longest.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById("btn1");
-    var in1 = document.getElementById("input1");
-    var sp1 = document.getElementById("sp1");
-    btn.onclick = function(){
-        var vals = in1.value.split(' ');
-        var maxi = vals[0];
-        vals.forEach(function(v){ if(v.length>maxi.length) maxi = v;});
-        sp1.textContent = maxi + '   length:   ' + maxi.length;
-    }
