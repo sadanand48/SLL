7605fd2d196874ed51fd5bc0155d7c5a188e36ab
sadanand48 <sadanand.shenoy4898@gmail.com>
2018-12-29 19:54:29 +0530

    4

---
+++ sll/4/buttons.js
@@ -0,0 +1,22 @@
+function btn1()
+{
+n=document.getElementById('input1').value;
+if(isNaN(n)){
+alert("Please enter numbers only");
+}
+else{
+var result= n*2;
+document.getElementById('result').innerHTML= result;
+}
+}
+function btn2()
+{
+n=document.getElementById('input1').value;
+if(isNaN(n)){
+alert("Please enter numbers only");
+}
+else{
+var result= n*n;
+document.getElementById('result').innerHTML= result;
+}
+}
