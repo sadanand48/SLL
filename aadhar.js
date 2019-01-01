8f41d41bd68db75dd8b61787088cf5a93375daf1
Sadanand Shenoy <sadanand.shenoy4898@gmail.com>
2019-01-01 22:27:35 +0530

    Update aadhar.js

--- aadhar.js
+++ aadhar.js
@@ -1,17 +1,17 @@
 window.onload = () => {
-  const details = [
+   var details = 
     {
       name: 'Fortis',
       location:"Bangalore"
-    }
-    
-  ];
-
-  details.forEach((item, index) => {
-    
-	document.getElementById('h_name').innerHTML = details[index].name;
-      document.getElementById('h_loc').innerHTML = details[index].location;
-      
-    
-  });
+    };
+    document.getElementById('h_name').innerHTML = details.name;
+      document.getElementById('h_loc').innerHTML = details.location;
 }
+  
+
+ 
+    
+	
+    
+  
+
